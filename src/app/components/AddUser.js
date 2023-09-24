e1a3f1a9c2a05e80df7b5ce24f199af0d198481c
jenish96 <jenishborda@gmail.com>
2023-09-24 10:51:03 +0530

    #11 pass data b\w page

--- src/app/components/AddUser.js
+++ src/app/components/AddUser.js
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { addUser } from "../redux/slice"
+import Link from "next/link"
 
 const AddUser = () => {
     const [name, setName] = useState("")
@@ -14,9 +15,10 @@ const AddUser = () => {
     return (
         <div className="user">
             <h2>Add User</h2>
-            <br />
             <input type="text" className="input" placeholder="Enter your Name" onChange={(e) => setName(e.target.value)} />
             <button onClick={userDispatch} className="btn">Submit</button>
+
+            <Link className="btn" href={"/removeuser"}>Remove User</Link>
         </div>
     )
 }
---
+++ src/app/removeuser/page.js
@@ -0,0 +1,28 @@
+"use client"
+
+import { useDispatch, useSelector } from "react-redux"
+import { removeUser } from "../redux/slice"
+
+const Page = () => {
+    const data = useSelector((data) => data.users)
+    const dispatch = useDispatch()
+    console.log("dtata", data)
+    return (
+        <div className="user">
+            <h1>Remove User Page</h1>
+            <h2>Display User</h2>
+            {
+                data?.map((item, index) =>
+                    <div className="user-item" key={index}>
+                        <span>
+                            {item.name}
+                        </span>
+                        <button onClick={() => dispatch(removeUser(item.id))}>Delete</button>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default Page
